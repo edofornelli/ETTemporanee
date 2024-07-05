e357f66b9f5c49cb252baa55c9da4e7e721a8b9c
edofornelli <114942883+edofornelli@users.noreply.github.com>
2024-07-05 09:47:46 +0200

    Pubblicata repo
    scheletro progetto ed aggiunte dipendenze

---
+++ src/main/java/com/example/ettemporanee/EtTemporaneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ettemporanee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtTemporaneeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtTemporaneeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ettemporanee/EtTemporaneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ettemporanee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtTemporaneeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
